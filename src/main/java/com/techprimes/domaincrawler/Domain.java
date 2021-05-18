ada791a74707df8cb0219bfdae58b5f4387a575f
Damitha Kithmal <dkithmal@MacBook-Pro.local>
2021-05-18 10:26:21 +0800

    initial commit

---
+++ src/main/java/com/techprimes/domaincrawler/Domain.java
@@ -0,0 +1,24 @@
+package com.techprimes.domaincrawler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Domain {
+
+
+    private String domain;
+    private String create_date;
+    private String update_date;
+    private String country;
+    private boolean isDead;
+    private String A;
+    private String NS;
+    private String CNAME;
+    private String MX;
+    private String TXT;
+
+}
